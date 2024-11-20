a6906745ab31be62d81824a07d37377fcc949199
ali ilan <ali.ilan@ozu.edu.tr>
2024-11-20 16:28:34 +0300

    Equipment class'ı implement edildi.

--- src/main/java/com/springcourse/project/model/Equipment.java
+++ src/main/java/com/springcourse/project/model/Equipment.java
@@ -1,7 +1,39 @@
 package com.springcourse.project.model;
 
-import jakarta.persistence.Entity;
 
-@Entity
 public class Equipment {
+    private String name;
+    private double price;
+
+    public Equipment(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Equipment() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
