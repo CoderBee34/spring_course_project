31486ad59ae04f703e2ed0cd4910095450643d0f
ali ilan <ali.ilan@ozu.edu.tr>
2024-11-20 16:26:12 +0300

    Location class'ı implement edildi.

--- src/main/java/com/springcourse/project/model/Location.java
+++ src/main/java/com/springcourse/project/model/Location.java
@@ -1,7 +1,50 @@
 package com.springcourse.project.model;
 
-import jakarta.persistence.Entity;
 
-@Entity
 public class Location {
+    private int code;
+    private String name;
+    private String address;
+
+    public Location(int code, String name, String address) {
+        this.code = code;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Location() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
