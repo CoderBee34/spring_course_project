b1130f29e10326c2ccca94556a84723e6043d945
ali ilan <ali.ilan@ozu.edu.tr>
2024-11-27 15:30:14 +0300

    CarService.searchAvailableCars method implemented.

--- src/main/java/com/springcourse/project/repository/CarRepository.java
+++ src/main/java/com/springcourse/project/repository/CarRepository.java
@@ -1,9 +1,14 @@
 package com.springcourse.project.repository;
 
 import com.springcourse.project.model.Car;
+import com.springcourse.project.model.CarStatus;
+import com.springcourse.project.model.CarType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarRepository extends JpaRepository<Car,String> {
+    List<Car> findByStatusAndTypeAndTransmissionType(CarStatus status, CarType type, String transmissionType);
 }
--- src/main/java/com/springcourse/project/service/CarService.java
+++ src/main/java/com/springcourse/project/service/CarService.java
@@ -1,11 +1,29 @@
 package com.springcourse.project.service;
 
+import com.springcourse.project.dto.CarDTO;
+import com.springcourse.project.model.Car;
+import com.springcourse.project.model.CarStatus;
+import com.springcourse.project.model.CarType;
 import com.springcourse.project.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CarService {
     @Autowired
     private CarRepository carRepository;
+
+    public List<CarDTO> searchAvailableCars(String carType, String transmissionType) {
+        List<Car> queryResult = carRepository.findByStatusAndTypeAndTransmissionType(CarStatus.AVAILABLE, CarType.STANDARD, "Automatic");
+        List<CarDTO> returnValue = queryResult.stream().map(car -> new CarDTO(
+                car.getBrand(),
+                car.getModel(),
+                car.getType(),
+                car.getMileage(),
+                car.getTransmissionType(),
+                car.getBarcode())).toList();
+        return returnValue;
+}
 }
