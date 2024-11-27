076d2b2af69f8f84be3402e2fa2a3487d16aafdb
ali ilan <ali.ilan@ozu.edu.tr>
2024-11-27 15:15:28 +0300

    CarDTO implemented.

---
+++ src/main/java/com/springcourse/project/dto/CarDTO.java
@@ -0,0 +1,72 @@
+package com.springcourse.project.dto;
+
+import com.springcourse.project.model.CarType;
+
+public class CarDTO {
+    private String brand;
+    private String model;
+    private CarType carType;
+    private double mileage;
+    private String transmissionType;
+    private String barcode;
+
+    public CarDTO() {
+    }
+
+    public CarDTO(String brand, String model, CarType carType, double mileage, String transmissionType, String barcode) {
+        this.brand = brand;
+        this.model = model;
+        this.carType = carType;
+        this.mileage = mileage;
+        this.transmissionType = transmissionType;
+        this.barcode = barcode;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public void setCarType(CarType carType) {
+        this.carType = carType;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+}
