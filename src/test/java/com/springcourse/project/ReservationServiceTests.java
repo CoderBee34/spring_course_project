c670b25639bc18fb5291cf6175eea6527857532b
ali ilan <ali.ilan@ozu.edu.tr>
2024-11-29 23:48:29 +0300

    base test need added.

--- src/test/java/com/springcourse/project/ReservationServiceTests.java
+++ src/test/java/com/springcourse/project/ReservationServiceTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest(classes = ProjectApplication.class)
 @ActiveProfiles("test")
 public class ReservationServiceTests {
