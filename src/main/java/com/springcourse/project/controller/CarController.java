cbdb8f388403281428808fe7bf2becbbb351979c
ali ilan <ali.ilan@ozu.edu.tr>
2024-12-02 00:18:46 +0300

    unnecessery controller methods deleted.

--- src/main/java/com/springcourse/project/controller/CarController.java
+++ src/main/java/com/springcourse/project/controller/CarController.java
@@ -1,9 +1,7 @@
 package com.springcourse.project.controller;
 
-import com.springcourse.project.dto.AvailableCarDTO;
 import com.springcourse.project.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +12,5 @@ import java.util.List;
 public class CarController {
     @Autowired
     CarService carService;
-
-    @GetMapping("/all")
-    public List<AvailableCarDTO> availableCars(){
-        return carService.searchAvailableCars("Standard", "Automatic");
-    }
 
 }
