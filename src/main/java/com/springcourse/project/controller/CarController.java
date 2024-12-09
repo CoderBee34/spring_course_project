340530dc1e438f47503e9d4c98b7df522e33f935
ali ilan <ali.ilan@ozu.edu.tr>
2024-12-09 15:35:02 +0300

    searchAvailableCars method in CarController class implemented.

--- src/main/java/com/springcourse/project/controller/CarController.java
+++ src/main/java/com/springcourse/project/controller/CarController.java
@@ -1,8 +1,13 @@
 package com.springcourse.project.controller;
 
+import com.springcourse.project.dto.AvailableCarDTO;
 import com.springcourse.project.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,5 +17,11 @@ import java.util.List;
 public class CarController {
     @Autowired
     CarService carService;
+
+    @RequestMapping(value="/available", method = RequestMethod.POST)
+    public ResponseEntity<List<AvailableCarDTO>> searchAvailableCars(@RequestBody String carType, @RequestBody String transmissionType) {
+        List<AvailableCarDTO> availableCarDTOList=carService.searchAvailableCars(carType,transmissionType);
+        return ResponseEntity.status(HttpStatus.FOUND).body(availableCarDTOList);
+    }
 
 }
