4be0b755ba15e9842405479c9d3244dd714e88d6
ali ilan <ali.ilan@ozu.edu.tr>
2024-12-09 15:37:35 +0300

    ReservationStatus Enum class updated.

--- src/main/java/com/springcourse/project/model/ReservationStatus.java
+++ src/main/java/com/springcourse/project/model/ReservationStatus.java
@@ -14,12 +14,12 @@ public enum ReservationStatus {
     private static final Map<String, ReservationStatus> stringToStatusMap = new HashMap<>();
 
     static {
-        stringToStatusMap.put("Active", ACTIVE);
-        stringToStatusMap.put("Pending", PENDING);
-        stringToStatusMap.put("Confirmed", CONFIRMED);
-        stringToStatusMap.put("Completed", COMPLETED);
-        stringToStatusMap.put("Cancelled", CANCELLED);
-        stringToStatusMap.put("None", NONE);
+        stringToStatusMap.put("active", ACTIVE);
+        stringToStatusMap.put("pending", PENDING);
+        stringToStatusMap.put("confirmed", CONFIRMED);
+        stringToStatusMap.put("completed", COMPLETED);
+        stringToStatusMap.put("cancelled", CANCELLED);
+        stringToStatusMap.put("none", NONE);
     }
     public static ReservationStatus fromString(String status) {
         return stringToStatusMap.get(status);
